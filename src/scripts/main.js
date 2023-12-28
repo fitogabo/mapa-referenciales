990cde475dbe929d21061c7e7c31f4db0b549720
gapantoj <36498275+fitogabo@users.noreply.github.com>
2023-12-28 10:01:43 -0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,48 +1,65 @@
+// main.js
 // Importaciones en la parte superior del archivo
-import { auth } from './firebase.js';
+import mapboxgl from 'mapbox-gl';
+import { observeAuthState, signOutUser } from './auth.js';
 import { readExcelFile } from './excelReader.js';
-import { loadToMapbox } from './MapboxUtility.js';
+import { initMapbox, loadToMapbox, hideMap } from './MapboxUtility.js';
 
-// Token de acceso a Mapbox
-const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZ2Fib3BhbnRlcmEiLCJhIjoiY2xlZG52M3R2MDV4ZDNvbXJwN2hlZXZmMCJ9.T4YSPRr9Hbxp0ID5-NLkCg';
+let currentUser = null;
+let initMapboxDebounced;
+let geojsonData;
 
-// Esta función genera el mapa después de que se autentica el usuario
-function initMapbox() {
-    mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [-73.24589, -39.81959],
-        zoom: 9
-    });
-}
-
-// Ejemplo de función de autenticación y carga de mapa
+// Maneja la autenticación y carga del mapa
 function initApp() {
-    auth.onAuthStateChanged(user => {
-        if (user) {
-            initMapbox();
-            console.log('Usuario conectado:', user.email);
+    observeAuthState(user => {
+        currentUser = user;
+        clearTimeout(initMapboxDebounced);
+        initMapboxDebounced = setTimeout(() => {
+            if (user) {
+                console.log('Usuario conectado:', user.email);
+                initMapbox();
+                document.getElementById('map').style.display = 'block';
+            } else {
+                console.log('Usuario no conectado o sesión cerrada.');
+                hideMap();
+            }
+        }, 250); // Debounce por 250 milisegundos
+    });
+
+    const signOutButton = document.getElementById('signOutButton');
+    if (signOutButton) {
+        signOutButton.addEventListener('click', signOutUser);
+    }
+
+    // Input de carga de archivo
+    const fileInput = document.getElementById('file-upload');
+    const loadButton = document.getElementById('load-file');
+
+    // Este event listener maneja la selección y la carga del archivo
+    fileInput.addEventListener('change', e => {
+        const file = e.target.files[0];
+        if (file) {
+            console.log('Archivo seleccionado:', file.name);
+            readExcelFile(file)
+                .then(data => {
+                    geojsonData = data; // Almacena los datos para cargarlos después
+                    console.log('Lectura de archivo Excel completa. Datos procesados:', geojsonData);
+                })
+                .catch(error => console.error('Error al leer el archivo:', error));
         } else {
-            console.log('Usuario no conectado o sesión cerrada.');
+            console.log('No se seleccionó ningún archivo.');
+        }
+    });
+
+    // Este event listener maneja la carga del archivo al mapa
+    loadButton.addEventListener('click', () => {
+        if (currentUser && geojsonData) {
+            loadToMapbox(geojsonData);
+        } else {
+            alert('Debes iniciar sesión y seleccionar un archivo para cargar datos.');
         }
     });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Inicializar aplicación al cargar el DOM
-    initApp();
-
-    // Configurar el oyente del evento de cambio para el input del archivo
-    const fileInput = document.getElementById('file-upload'); // importante que el ID coincida con el ID de index.html
-    if (fileInput) {
-        fileInput.addEventListener('change', (e) => {
-            const file = e.target.files[0];
-            if (file) {
-                readExcelFile(file);
-            }
-        });
-    } else {
-        console.error('El elemento de entrada del archivo no existe en el DOM');
-    }
-});
+// Event listener para cuando el contenido del DOM esté totalmente cargado
+document.addEventListener('DOMContentLoaded', initApp);
